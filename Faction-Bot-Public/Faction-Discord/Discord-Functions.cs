83162d95569ecd5f01dd4f12ed15e177e9c371b6
IsaacJuracich <boddyrock3@gmail.com>
2021-11-01 11:25:35 -0700

    Commit

--- Faction-Bot-Public/Faction-Discord/Discord-Functions.cs
+++ Faction-Bot-Public/Faction-Discord/Discord-Functions.cs
@@ -8,19 +8,29 @@ using System.Threading.Tasks;
 
 namespace Faction_Bot_Public.Faction_Discord {
     public class Discord_Functions {
-        public static List<string> DisplayNodesRecursive(IEnumerable<INode> nodes, INode parent, int level = 0) {
+        public static List<string> displayNodesRecursive(IEnumerable<INode> nodes, INode parent, int level = 0) {
             List<string> value = new List<string>();
             IEnumerable<INode> children = nodes.Where(x => x.ParentId == parent.Id);
             foreach (INode child in children) {
                 string infos = $"- {child.Name} - {child.Size} bytes - {child.CreationDate}";
                 value.Add(infos.PadLeft(infos.Length + level, '\t'));
                 if (child.Type == NodeType.Directory)
-                    DisplayNodesRecursive(nodes, child, level + 1);
+                    displayNodesRecursive(nodes, child, level + 1);
             }
             return value;
         }
         public static EmbedBuilder embed() {
             return new EmbedBuilder().WithColor(71, 108, 215).WithCurrentTimestamp();
+        }
+        public static bool doesConfigExist(ulong gId) {
+            Uri folderLink = new Uri("https://mega.nz/folder/8d1GQbBL#Os6d2DBL2rgwEzNOEMhUsA");
+            IEnumerable<INode> nodes = Discord_Bot.client.GetNodesFromLink(folderLink);
+            foreach (INode node in nodes.Where(x => x.Type == NodeType.File)) {
+                if (node.Name == $"{gId}.ini") {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
