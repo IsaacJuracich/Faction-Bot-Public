b73f584025fd57da07954f8361dcc71a425a6885
IsaacJuracich <boddyrock3@gmail.com>
2021-11-01 01:27:17 -0700

    Progress

--- Faction-Bot-Public/Faction-Discord/Discord-Functions.cs
+++ Faction-Bot-Public/Faction-Discord/Discord-Functions.cs
@@ -1,4 +1,5 @@
-﻿using Discord;
+﻿using CG.Web.MegaApiClient;
+using Discord;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,5 +8,16 @@ using System.Threading.Tasks;
 
 namespace Faction_Bot_Public.Faction_Discord {
     public class Discord_Functions {
+        public static List<string> DisplayNodesRecursive(IEnumerable<INode> nodes, INode parent, int level = 0) {
+            List<string> value = new List<string>();
+            IEnumerable<INode> children = nodes.Where(x => x.ParentId == parent.Id);
+            foreach (INode child in children) {
+                string infos = $"- {child.Name} - {child.Size} bytes - {child.CreationDate}";
+                value.Add(infos.PadLeft(infos.Length + level, '\t'));
+                if (child.Type == NodeType.Directory)
+                    DisplayNodesRecursive(nodes, child, level + 1);
+            }
+            return value;
+        }
     }
 }
